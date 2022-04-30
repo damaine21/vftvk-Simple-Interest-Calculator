4aad9697b69bbdfea4f9c5fd93e6c0b8094943c4
damaine21 <36740574+damaine21@users.noreply.github.com>
2022-04-30 16:29:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 function compute()
 {
+    // Simple Interest Formula
     p = document.getElementById("principal").value;
     r = document.getElementById("rate").value;
     y = document.getElementById("years").value;
     
-    i = p(1 + (r*y));
+    i = p * r * y;
     
+    
+    // Figure out the number of years
+    currentDate = new Date();
+    let year = currentDate.getFullYear();
+    numYears = currentDate + y;
+    
+    
+    // Output the results
     output = "If you deposit " + p + ",<br>" +
              "at an interest rate of " + r + "%.<br>" +
              "You will receive an amount of " + i + ",<br>" +
-             "in the year " + y;
+             "in the year " + numYears;
     
     document.getElementById("output").innerHTML(output);
+    
+
 }
         
