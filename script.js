b68bc9b918b9b48a35a1279713e753e88ab5663a
damaine21 <36740574+damaine21@users.noreply.github.com>
2022-04-30 16:19:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,13 @@ function compute()
     r = document.getElementById("rate").value;
     y = document.getElementById("years").value;
     
+    i = p(1 + (r*t));
+    
+    output = "If you deposit " + p + ",<br>" +
+             "at an interest rate of " + r + "%.<br>" +
+             "You will receive an amount of " + i + ",<br>" +
+             "in the year " + y;
+    
+    document.getElementById("output").innerHTML(output);
 }
         
